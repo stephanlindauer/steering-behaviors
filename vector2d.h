3d38df647d4c0fd57888daad8af8b29f545ab52b
stephan.lindauer <stephan.lindauer@gmail.com>
2012-07-03 17:28:05 +0200

    fixes + formatting

--- vector2d.h
+++ vector2d.h
@@ -1,64 +1,69 @@
 #ifndef VECTOR2D_H
 #define VECTOR2D_H
 
+#include <math.h>
+#include <algorithm>
 
+using namespace std;
 
 class Vector2D
 {
+
 private :
+
     float _x;
     float _y;
+
+public:
 
     Vector2D(float x, float y) :
         _x(x),
         _y(y) {}
 
-    Vector2D    clone()
+    Vector2D clone()
     {
         return Vector2D(x, y);
     }
 
-
-    void    zero()
+    void zero()
     {
         _x = 0;
         _y = 0;
         return this;
     }
 
-
     bool isZero()
     {
         return (_x == 0) && (_y == 0);
     }
 
-    setLength(float value)
+    void setLength(float value)
     {
         float a = angle;
-        _x = Math.cos(a) * value;
-        _y = Math.sin(a) * value;
+        _x = cos(a) * value;
+        _y = sin(a) * value;
     }
 
-    float     getLength()
+    float getLength()
     {
-        return Math.sqrt(lengthSQ);
+        return sqrt(lengthSQ);
     }
 
-    float    getLengthSQ()
+    float getLengthSQ()
     {
         return (_x * _x) + (_y * _y);
     }
 
-    void    setAngle(float value)
+    void setAngle(float value)
     {
         float len = getLength();
-        _x = Math.cos(value) * len;
-        _y = Math.sin(value) * len;
+        _x = cos(value) * len;
+        _y = sin(value) * len;
     }
 
-    float  getAngle()
+    float getAngle()
     {
-        return Math.atan2(_y, _x);
+        return atan2(_y, _x);
     }
 
     Vector2D normalize()
@@ -74,9 +79,9 @@ private :
         return this;
     }
 
-    Vector2D  truncate(float max)
+    Vector2D truncate(float max)
     {
-        length = Math.min(max, length);
+        length = min(max, length);
         return this;
     }
 
@@ -97,15 +102,19 @@ private :
         return _x * v2.x + _y * v2.y;
     }
 
-    crossProd(Vector2D v2)
+    void crossProd(Vector2D v2)
     {
         return _x * v2.y - _y * v2.x;
     }
 
     float  angleBetween(Vector2D v1, Vector2D v2)
     {
-        if(!v1.isNormalized()) v1 = v1.clone().normalize();
-        if(!v2.isNormalized()) v2 = v2.clone().normalize();
+        if(!v1.isNormalized()) {
+            v1 = v1.clone().normalize();
+        }
+        if(!v2.isNormalized()){
+            v2 = v2.clone().normalize();
+        }
         return Math.acos(v1.dotProd(v2));
     }
 
@@ -114,11 +123,10 @@ private :
         return perp.dotProd(v2) < 0 ? -1 : 1;
     }
 
-    Vector2D    getPerp()
+    Vector2D getPerp()
     {
         return new Vector2D(-y, x);
     }
-
 
     float dist(Vector2D v2)
     {
@@ -137,12 +145,10 @@ private :
         return new Vector2D(_x + v2.x, _y + v2.y);
     }
 
-    Vector2D    subtract(Vector2D v2)
+    Vector2D subtract(Vector2D v2)
     {
         return new Vector2D(_x - v2.x, _y - v2.y);
     }
-
-
 
     Vector2D multiply(float value)
     {
@@ -154,37 +160,30 @@ private :
         return new Vector2D(_x / value, _y / value);
     }
 
-    bool   equals(Vector2D v2)
+    bool equals(Vector2D v2)
     {
         return _x == v2.x && _y == v2.y;
     }
 
-    void setX(float value):void
+    void x(float value)
     {
         _x = value;
     }
 
-    float  getX()    {
+    float x()
+    {
         return _x;
     }
 
-    void  setY(float value)
+    void y(float value)
     {
         _y = value;
     }
-    float getY()
+    float y()
     {
         return _y;
     }
 
 }
-
-
-
-
-
-
-
-
 
 #endif // VECTOR2D_H
