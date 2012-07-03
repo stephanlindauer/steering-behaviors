e121f4170676f7ff8f51e4e5ac4372c1df0cfada
tiberius <tiberius@ti-mobile.(none)>
2012-07-03 21:38:45 +0200

    eeek

--- map.h
+++ map.h
@@ -5,17 +5,42 @@
 #include "object.h"
 #include "bird.h"
 #include "obstacle.h"
+#include "nullobject.h"
 #include "vector2d.h"
 
 class Map {
 public:
-    Map(unsigned int width = 10, unsigned int height = 10) {
-        m_objects.reserve(width * height);
+    Map(unsigned int width = 10, unsigned int height = 10) :
+        m_width(width),
+        m_height(height) {
+        m_objects.reserve(m_width * m_height);
+        m_objects.insert(0, m_width * m_height, NULL_OBJECT);
+    }
+
+    void addObject(Object * o) {
+        m_objects.replace((o->m_position), o);
+    }
+
+    void delObject(Object * o) {
+        m_objects.replace(pos(o->m_position), NULL_OBJECT);
     }
 
 private:
+    unsigned int m_width;
+    unsigned int m_height;
+    QVector<Object *> m_objects;
 
-    QVector<Object> m_objects;
+    unsigned int x(const Vector2D vector) {
+        return (int) vector.x();
+    }
+
+    unsigned int y(const Vector2D vector) {
+        return (int) vector.y();
+    }
+
+    unsigned int pos(const Vector2D vector) {
+        return y(vector) * m_width + y(vector);
+    }
 
 };
 
--- nullobject.h
+++ nullobject.h
@@ -1,4 +1,12 @@
 #ifndef NULLOBJECT_H
 #define NULLOBJECT_H
 
+#include "object.h"
+
+class NullObject : public Object {
+
+};
+
+const NullObject * NULL_OBJECT = new NullObject();
+
 #endif // NULLOBJECT_H
