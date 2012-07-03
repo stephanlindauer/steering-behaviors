890ab345eb268ae9b58e78337178be4df32cab7d
stephan.lindauer <stephan.lindauer@gmail.com>
2012-07-03 20:46:29 +0200

    blabla

--- mapdrawer.h
+++ mapdrawer.h
@@ -3,23 +3,31 @@
 
 #include <QtGui>
 #include <birddrawer.h>
+#include <map.h>
 
 class MapDrawer : public Drawer
 {
 public:
 
-    MapDrawer(QVector birds){
-        this->birds = birds;
-    }
+    MapDrawer(QVector birds , QVector obstacles, Map map) :
+        m_birds(birds),
+        m_obstacles(obstacles),
+        m_map(map){}
+
 
     void draw (const QGraphicsScene *scene){
-        foreach (BirdDrawer birdDrawer, birds){
-            birdDrawer.draw(scene);
+        for (int i = 0; i < birds.size(); i++) {
+            m_birds[i].draw(scene);
+        }
+        for (int j = 0; j < m_obstacles.size(); j++) {
+            m_birds[i].draw(scene);
         }
     }
 
 private:
-        QVector<BirdDrawer> birds ;
+        QVector<BirdDrawer> m_birds ;
+        QVector<ObstacleDrawer> m_obstacles;
+        Map m_map;
 };
 
 
