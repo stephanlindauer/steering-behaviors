a79820fe8cc372dae05be8d9106775580d3d7891
tiberius <tiberius@ti-mobile.(none)>
2012-07-04 02:48:32 +0200

    so, alles weg und von vorn

--- model/sector.h
+++
@@ -1,5 +0,0 @@
-#ifndef SECTOR_H
-#define SECTOR_H
-
-
-#endif // SECTOR_H
--- view/drawer.h
+++
@@ -1,9 +0,0 @@
-#ifndef DRAWER_H
-#define DRAWER_H
-
-class Drawer {
-public:
-    virtual void draw(const QGraphicsScene * scene);
-};
-
-#endif // DRAWER_H
