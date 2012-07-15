f188b352449ca74599254e6435df023ae731a8c3
tiberius <leckmich@m-ars.ch>
2012-07-15 15:43:13 +0200

    some last changes i dont remember atm

--- model/random.cpp
+++ model/random.cpp
@@ -1,3 +1,76 @@
 #include "model/random.h"
+#include <QObject>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+#include <QUrl>
 
-//static steering_behaviors::Random * INSTANCE = new steering_behaviors::Random();
+class RandomSource : public QObject {
+
+    Q_OBJECT
+
+private:
+
+    QNetworkAccessManager * m_manager;
+    bool m_useRandomOrg;
+    QList<int> m_store;
+
+public:
+
+    RandomSource():
+        m_manager(new QNetworkAccessManager()),
+        m_useRandomOrg(false),
+        m_store(QList<int>()) {}
+
+    ~RandomSource() {
+        if (m_manager != NULL)
+            delete m_manager;
+    }
+
+public slots:
+
+    inline void update(QNetworkReply * reply) {
+
+    }
+
+    inline void setInternal(void) {
+        m_useRandomOrg = false;
+    }
+
+    inline void setRandomOrg(void) {
+        m_useRandomOrg = true;
+    }
+
+private:
+
+    inline void refreshStore(void) {
+        QUrl url("http://www.random.org/integers/?num=10000&min=1&max=500&col=1&base=10&format=plain&rnd=new");
+        QNetworkRequest request(url);
+        if (m_manager == NULL)
+            m_manager = new QNetworkAccessManager();
+
+        connect(m_manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(update(QNetworkReply *)));
+        m_manager->get(request);
+
+    }
+
+};
+
+//RandomSource SOURCE;
+
+int steering_behaviors::Random::random(int limit) {
+    //RandomSource src;
+    /*if (INSTANCE->m_useRandomOrg) {
+        //
+    }*/
+
+    return rand() % limit;
+}
+
+void steering_behaviors::Random::setInternal(void) {
+    //SOURCE::setInternal();
+}
+
+void steering_behaviors::Random::setRandomOrg(void) {
+    //SOURCE::setRandomOrg();
+}
--- model/random.h
+++ model/random.h
@@ -1,68 +1,16 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
-#include <stdlib.h>
-#include <QObject>
-#include <QtNetwork/QNetworkAccessManager>
-#include <QtNetwork/QNetworkRequest>
-#include <QtNetwork/QNetworkReply>
-#include <QUrl>
-
 namespace steering_behaviors {
 
-class Random /* : public QObject*/ {
-
-    /*Q_OBJECT
+class Random {
 
 public:
-    Random():m_manager(NULL),m_useRandomOrg(false),m_store(QList<int>()) {
+    static int random(int limit);
+    static void setInternal(void);
+    static void setRandomOrg(void);
 
-    }
-*/
-public:
-
-    static int random(int limit) {
-        /*if (steering_behaviors::INSTANCE->m_useRandomOrg) {
-            //
-        }*/
-
-        return rand() % limit;
-    }
-/*
-    static void setInternal(void) {
-        steering_behaviors::INSTANCE->m_useRandomOrg = false;
-    }
-
-    static void setRandomOrg(void) {
-        steering_behaviors::INSTANCE->m_useRandomOrg = true;
-    }
-
-public slots:
-
-    void update(QNetworkReply * reply) {
-
-    }
-
-private:
-
-    void refreshStore(void) {
-        QUrl url("http://www.random.org/integers/?num=10000&min=1&max=500&col=1&base=10&format=plain&rnd=new");
-        QNetworkRequest request(url);
-        if (m_manager == NULL)
-            m_manager = new QNetworkAccessManager();
-
-        connect(m_manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(update(QNetworkReply *)));
-        m_manager->get(request);
-
-    }
-
-    QNetworkAccessManager * m_manager;
-    bool m_useRandomOrg;
-    QList<int> m_store;
-*/
 };
-
-/*static Random * INSTANCE;*/
 
 }
 
